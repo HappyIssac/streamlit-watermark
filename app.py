bdda58fb2e40ac8e2806e0fe4635c902e7acc6e3
HappyIssac <Issacayaz123@gmail.com>
2025-04-08 06:26:15 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from PIL import Image
+
+st.title("Watermarking App")
+
+uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
